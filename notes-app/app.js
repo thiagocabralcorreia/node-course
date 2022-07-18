520a80a08042bb2f94fd184eb6e7481abb884332
Thiago Cabral Correia <thiagocabralcorreia@gmail.com>
2022-07-18 14:51:00 -0300

    Add npm modules and global nodemon

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,13 @@
+const validator = require('validator');
+const chalk = require('chalk')
+
 const getNotes = require('./notes.js');
 
 const msg = getNotes();
 
 console.log(msg);
+
+console.log(validator.isEmail('tcc@email.com'));
+
+const greenMsg = chalk.bold.green('Success!')
+console.log(greenMsg)
